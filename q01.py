ce82b88c83a9263346ac45d7404b6f37f5293fbc
mauribelu <mauribelu@unal.edu.co>
2019-05-27 19:02:51 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -2,3 +2,9 @@
 ## Imprima la cantidad de registros por cada letra 
 ## de la columna _c1 de la tabla tbl0
 ## 
+## Leer el archivo
+x = pd.read_csv('tbl0.tsv', sep = '\t')
+x.head()
+
+## Cantidad de registros por cada letra
+x['_c1'].value_counts()
