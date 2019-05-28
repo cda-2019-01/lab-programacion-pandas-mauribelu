2969606ecba87e57c5649774f5de2bd4f52573b1
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2019-05-27 22:02:22 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -3,7 +3,9 @@
 ## de la columna _c1 de la tabla tbl0
 ## 
 import pandas as pd
+import numpy as np
 ## Leer el archivo
 x = pd.read_csv('tbl0.tsv', sep = '\t')
 ## Cantidad de registros por cada letra
 x.groupby'(_c1').count()['_c0']
+print(x)
