e4d00996535d967ad57d07418303043ce307b1c7
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2019-05-27 23:39:43 -0500

    Update q06.py

--- q06.py
+++ q06.py
@@ -3,7 +3,7 @@
 ## de la tabla tbl0
 import pandas as pd
 import numpy as np
-## Leer archivo
+## Leer tabla
 x = pd.read_csv('tbl0.tsv', sep = '\t')
 ## Impresion suma _c2
 x = x.groupby('_c1').sum()['_c2']
