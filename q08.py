558e66fba2adbab70e8ac66516f626d8cc257571
mauribelu <mauribelu@unal.edu.co>
2019-05-27 19:57:32 -0500

    Update q08.py

--- q08.py
+++ q08.py
@@ -1,4 +1,9 @@
 ##
 ## Agregue el año como una columna a la tabla tbl0.tsv 
 ## 
+##Lectura de tabla
+x = pd.read_csv('tbl0.tsv', sep = '\t')
+##Agrega el año como una columna al final
+x['year'] = [i.split('-')[0] for i in x['_c3']]
+x.head()
 
