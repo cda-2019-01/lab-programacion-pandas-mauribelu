d8adfdbe25385b946ad14821a18b2dece4d772a4
mauribelu <mauribelu@unal.edu.co>
2019-05-27 19:17:44 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -2,4 +2,9 @@
 ## Imprima el maximo de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
+##Lectura de la tabla
+x = pd.read_csv('tbl0.tsv', sep = '\t')
+
+## Imprime el máximo
+x.groupby ('_c1').max()[['_c2']]
 
