5cde3e7fd3acc97cdcbc37bc38912d6f9471c8e3
mauribelu <mauribelu@unal.edu.co>
2019-05-27 20:17:35 -0500

    Update q10.py

--- q10.py
+++ q10.py
@@ -3,4 +3,12 @@
 ## separada por ',' de los valores de la columna _c4
 ## de la tabla tbl1.tsv
 ## 
+##Leer tabla
+x1 = pd.read_csv('tbl1.tsv', sep = '\t')
+#construcción de tabla
+x1temp = x1.groupby('_c0')['_c4'].apply(list)
+x1b = pd.DataFrame()
+x1b['_c0'] = x1temp.keys()
+x1b['lista'] = [elem for elem in x1temp]
+x1b['lista'] = [",".join(str(v) for v in sorted(elem)) for elem in x1b['lista']]
 
