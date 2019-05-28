2b4042fd60d934d62363018a033875c6fb7bf060
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2019-05-27 23:35:38 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -3,9 +3,8 @@
 ## de la tabla tbl0
 import pandas as pd
 import numpy as np
-## Lectura de la tabla 
+## Leer Tabla 
 x = pd.read_csv('tbl0.tsv', sep = '\t')
-##
 ## Promedio de la _c2 por cada letra de la _c1
 x = x.groupby('_c1').mean()['_c2']
 print(x)
