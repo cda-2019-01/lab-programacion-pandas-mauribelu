86641db8307463f0d7309f6f427aea8ef6038865
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2019-05-27 21:50:36 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -2,6 +2,7 @@
 ## Imprima el promedio de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
+import pandas as pd
 ##Lectura de archivo
 x = pd.read_csv('ltbl0.tsv', sep = '\t')
 ##Imprimir Promedio
