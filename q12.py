cf504c8f07bb37e808d092a72dbea29708910a81
mauribelu <mauribelu@unal.edu.co>
2019-05-27 20:54:34 -0500

    Update q12.py

--- q12.py
+++ q12.py
@@ -2,6 +2,7 @@
 ## Si la columna _c0 es la clave en las tablas tbl0.tsv
 ## y tbl2.tsv, compute la suma de tbl2._c5b por cada
 ## valor en tbl0._c1.
-## 
-
-
+## lectura de tabla
+x2 = pd.read_csv('tbl2.tsv', sep = '\t')
+##suma de tabla 
+x2.groupby('_c5a')['_c5b'].sum()
