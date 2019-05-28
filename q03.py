900883477a92a846b2ee8a46f61ca433785bcf86
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2019-05-27 22:03:36 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -3,8 +3,10 @@
 ## de la tabla tbl0
 ## 
 import pandas as pd
+import numpy as np
 ##Lectura de la tabla
 x = pd.read_csv('tbl0.tsv', sep = '\t')
 ## Imprime el máximo
 x.groupby ('_c1').max()['_c2']
+print(x)
 
