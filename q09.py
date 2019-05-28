6ed39afdbcf531447c1601f34264e46ac01f210e
mauribelu <mauribelu@unal.edu.co>
2019-05-27 20:08:57 -0500

    Update q09.py

--- q09.py
+++ q09.py
@@ -3,5 +3,13 @@
 ## separada por ':' de los valores de la columna _c2
 ## para el archivo tbl0.tsv
 ## 
+##Leer archivo
+x = pd.read_csv('tbl0.tsv', sep = '\t')
+##Construcción de tabla
+xtemp = x.groupby('_c1')['_c2'].apply(list)
+xb = pd.DataFrame()
+xb['_c1'] = xtemp.keys()
+xb['_c2'] = [elem for elem in xtemp]
+xb['_c2'] = [":".join(str(v) for v in sorted(elem)) for elem in xb['_c2']]
 
 
