f2f18df8fb5ed1aa565de6f1c0f5a00ffd8645c0
mauribelu <mauribelu@unal.edu.co>
2019-05-27 19:39:10 -0500

    Update q04.py

--- q04.py
+++ q04.py
@@ -2,5 +2,15 @@
 ## Imprima los valores unicos e la columna _c4 de 
 ## de la tabla tbl1 en mayusculas
 ## 
+## Lectura de tabla
+x1 = pd.read_csv('tbl1.tsv', sep = '\t')
+
+##Imprimir Valores únicos
+unicos = x1['_c4'].unique()
+resultado = []
+for x in unicos:
+    resultado.append(x.upper())
+resultado = sorted(resultado)
+resultado
 
 
