1f933906d8101dbaae7c1a2f1a2da05f3349c268
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2019-05-27 23:41:02 -0500

    Update q08.py

--- q08.py
+++ q08.py
@@ -2,7 +2,7 @@
 ## Agregue el año como una columna a la tabla tbl0.tsv
 import pandas as pd
 import numpy as np
-## llamar archivo
+## Leer tabla
 x = pd.read_csv('tbl0.tsv', sep = '\t')
 ## Agregar el año en una columna al final
 x['ano'] = [i.split('-')[0] for i in x['_c3']]
