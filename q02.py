4f6178e39d57909323058f7a0c71aaaf28dca380
mauribelu <mauribelu@unal.edu.co>
2019-05-27 19:16:01 -0500

    Update q02.py

--- q02.py
+++ q02.py
@@ -2,7 +2,7 @@
 ## Imprima el promedio de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
-##Lectura
+##Lectura de archivo
 x = pd.read_csv('ltbl0.tsv', sep = '\t')
-##Promedio
+##Imprimir Promedio
 x.groupby(['_c1'])['_c2'].mean()
