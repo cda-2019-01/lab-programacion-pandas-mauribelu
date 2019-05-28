4bc22be315414c361abc81b0cc80f3c59e01d5ed
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2019-05-27 21:46:38 -0500

    Update q01.py

--- q01.py
+++ q01.py
@@ -3,8 +3,6 @@
 ## de la columna _c1 de la tabla tbl0
 ## 
 ## Leer el archivo
-x = pd.read_csv('tbl0.tsv', sep = '\t')
-x.head()
-
+##x = pd.read_csv('tbl0.tsv', sep = '\t')
 ## Cantidad de registros por cada letra
-x['_c1'].value_counts()
+x.groupby'(_c1').count()['_c0']
