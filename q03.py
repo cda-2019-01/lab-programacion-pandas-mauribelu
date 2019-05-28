dd4e6ec144231df807e9345c6d588822585811cb
Madelin Uribe Lujan <50299415+mauribelu@users.noreply.github.com>
2019-05-27 21:51:14 -0500

    Update q03.py

--- q03.py
+++ q03.py
@@ -2,6 +2,7 @@
 ## Imprima el maximo de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
+import pandas as pd
 ##Lectura de la tabla
 x = pd.read_csv('tbl0.tsv', sep = '\t')
 ## Imprime el máximo
