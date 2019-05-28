7a6501c924c3e64341b2feb59a34843ae3620564
mauribelu <mauribelu@unal.edu.co>
2019-05-27 19:50:52 -0500

    Update q06.py

--- q06.py
+++ q06.py
@@ -2,4 +2,9 @@
 ## Imprima la suma de la _c2 por cada letra de la _c1 
 ## de la tabla tbl0
 ## 
+##Leer tabla
+x = pd.read_csv('tbl0.tsv', sep = '\t')
+
+##Imprimir suma
+x.groupby(['_c1'])['_c2'].sum()
 
